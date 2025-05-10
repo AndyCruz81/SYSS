a5bf3c8d15729dee3052d17661d0c30326d43e86
Andy José Cruz García <80351895+AndyCruz81@users.noreply.github.com>
2025-05-10 14:06:54 -0600

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['http://192.168.0.82:3000', 'http://localhost:3000'],
+  /* otras opciones de configuración aquí */
+};
+
+export default nextConfig;
